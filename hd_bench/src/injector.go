ce46867f84074e4af513abf6a67e999fce21e3ec
Matthieu Velay <matthieu.velay@scality.com>
2019-02-07 11:18:27 +0100

    Fix operation option

--- hd_bench/src/injector.go
+++ hd_bench/src/injector.go
@@ -22,7 +22,7 @@ import (
 // performWorkload
 func performWorkload(
 	hdType string, // server or client
-	operation string, // PUT / GET / DELETE
+	operations string, // PUT / GET / DELETE
 	baseURL string,
 	keys []string, // list of keys
 	payloadFile string,
@@ -31,49 +31,52 @@ func performWorkload(
 	wg *sync.WaitGroup) {
 
 	client := utils.CustomClient(100) // HTTP client
+	opArray := strings.Split(operations, " ")
 
 	var keysGenerated []string
 	var totalSize int
 
 	start := time.Now()
 
-	// Loop on all keys
-	for _, key := range keys {
-		// Build request
-		glog.V(2).Info(operation, " key: ", key, " on ", baseURL)
-		opRequest := utils.OpKey(hdType, operation, key, payloadFile, size, baseURL)
-		res, err := client.Do(opRequest)
+	for _, operation := range opArray {
 
-		if err != nil {
-			log.Fatal("err=", err)
-		}
-		if res.StatusCode >= 300 {
-			log.Fatal("status code=", res.StatusCode, "res=", res)
-		}
-		// Compare PUT and GET answer
-		if operation == "get" {
-			comparison := compareGetPut(payloadFile, res)
-			if comparison == false {
-				log.Fatal("GET response body different from original PUT, expected:", payloadFile)
+		// Loop on all keys
+		for _, key := range keys {
+			// Build request
+			glog.V(2).Info(operation, " key: ", key, " on ", baseURL)
+			opRequest := utils.OpKey(hdType, operation, key, payloadFile, size, baseURL)
+			res, err := client.Do(opRequest)
+
+			if err != nil {
+				log.Fatal("err=", err)
 			}
-		} else {
-			io.Copy(ioutil.Discard, res.Body)
+			if res.StatusCode >= 300 {
+				log.Fatal("status code=", res.StatusCode, "res=", res)
+			}
+			// Compare PUT and GET answer
+			if operation == "get" {
+				comparison := compareGetPut(payloadFile, res)
+				if comparison == false {
+					log.Fatal("GET response body different from original PUT, expected:", payloadFile)
+				}
+			} else {
+				io.Copy(ioutil.Discard, res.Body)
+			}
+
+			// Close the current request
+			res.Body.Close()
+
+			// After a PUT on client hdproxy, get the generated key
+			if operation == "put" && hdType == "client" {
+				randomKey := res.Header.Get("Scal-Key")
+				keysGenerated = append(keysGenerated, randomKey)
+			} else {
+				keysGenerated = append(keysGenerated, key)
+			}
+
+			// Update total put size
+			totalSize += int(size)
 		}
-
-		// Close the current request
-		res.Body.Close()
-
-		// After a PUT on client hdproxy, get the generated key
-		if operation == "put" && hdType == "client" {
-			randomKey := res.Header.Get("Scal-Key")
-			keysGenerated = append(keysGenerated, randomKey)
-		} else {
-			keysGenerated = append(keysGenerated, key)
-		}
-
-		// Update total put size
-		totalSize += int(size)
-
 	}
 	if len(keysGenerated) != len(keys) {
 		fmt.Println("nr keys generated=", len(keysGenerated), "nr keys=", len(keys))
@@ -83,8 +86,6 @@ func performWorkload(
 	wg.Done()
 
 	chanThrpt <- getThroughput(start, totalSize)
-	//close(chanThrpt)
-	//return keysGenerated, throughput
 }
 
 func getThroughput(start time.Time, size int) float64 {
@@ -131,7 +132,7 @@ func getFileSize(path string) int {
 
 func mainFunc(
 	hdType string,
-	operations []string,
+	operations string,
 	baseserver string,
 	nrkeys int,
 	payloadFile string,
@@ -142,29 +143,14 @@ func mainFunc(
 	var wgWorkload sync.WaitGroup
 	size := getFileSize(payloadFile)
 
-	// put operation is mandatory
-	//keysPut, throughput = performWorkload(hdType, "put", baseserver, keys, payloadFile, size)
 	for i := 0; i < workers; i++ {
 		// generate nrkeys random keys
 		keys := utils.GenerateKeys(hdType, nrkeys)
 
 		wgWorkload.Add(1)
-		go performWorkload(hdType, "put", baseserver, keys, payloadFile, size, chanThrpt, &wgWorkload)
-
-		//thrpt["put"] = throughput
-
-		// Loop on optional operations 'GET' and/or 'DELETE'
-		for i := 0; i < len(operations); i++ {
-			// Perform operations
-			operation := operations[i]
-			if operation == "get" || operation == "del" {
-				//wgWorkload.Add(1)
-				//_, throughput = performWorkload(hdType, operations[i], baseserver, keysPut, payloadFile, size, wgWorkload)
-				//go performWorkload(hdType, operations[i], baseserver, keysPut, payloadFile, size, &wgWorkload)
-				//thrpt[operations[i]] = throughput
-			}
-		}
+		go performWorkload(hdType, operations, baseserver, keys, payloadFile, size, chanThrpt, &wgWorkload)
 	}
+
 	wgWorkload.Wait()
 	wgMain.Done()
 }
@@ -188,8 +174,6 @@ func main() {
 	flag.Parse()
 
 	chanThrpt := make(chan float64)
-	// Get operations to perform
-	operations := strings.Split(*operationsPtr, " ")
 
 	// Launch goroutines in a loop
 	for nrinstances := 0; nrinstances < *nrinstancesPtr; nrinstances++ {
@@ -199,7 +183,7 @@ func main() {
 			baseURL = baseURL + "proxy/arc/"
 		}
 		wgMain.Add(1)
-		go mainFunc(*typePtr, operations, baseURL, *nrkeysPtr, *payloadPtr, &wgMain, chanThrpt, *nrworkersPtr)
+		go mainFunc(*typePtr, *operationsPtr, baseURL, *nrkeysPtr, *payloadPtr, &wgMain, chanThrpt, *nrworkersPtr)
 
 	}
 
