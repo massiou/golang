fa75318df57db42fb92e099fc81693cc80e987c4
Matthieu Velay <matthieu.velay@scality.com>
2019-03-13 11:53:23 +0100

    Add expected code

--- hd_bench/src/injector_test.go
+++ hd_bench/src/injector_test.go
@@ -26,25 +26,26 @@ func TestOpKey(t *testing.T) {
 	size := int(fi.Size())
 
 	type test struct {
-		hdType    string
-		operation string
-		baseurl   string
-		key       string
-		file      string
-		fileSize  int
+		hdType      string
+		operation   string
+		baseurl     string
+		key         string
+		file        string
+		fileSize    int
+		expectedRes int
 	}
 
 	// Generate use cases for server
 	var testworkload []test
-	testworkload = append(testworkload, test{"server", "put", "http://127.0.0.1:4244/", "key0", fileTest, size})
-	testworkload = append(testworkload, test{"server", "get", "http://127.0.0.1:4244/", "key0", fileTest, size})
-	testworkload = append(testworkload, test{"server", "del", "http://127.0.0.1:4244/", "key0", fileTest, size})
+	testworkload = append(testworkload, test{"server", "put", "http://127.0.0.1:4244/", "key0", fileTest, size, http.StatusOK})
+	testworkload = append(testworkload, test{"server", "get", "http://127.0.0.1:4244/", "key0", fileTest, size, http.StatusOK})
+	testworkload = append(testworkload, test{"server", "del", "http://127.0.0.1:4244/", "key0", fileTest, size, http.StatusOK})
+	testworkload = append(testworkload, test{"client", "put", "http://127.0.0.1:4244/", "key0", fileTest, size, http.StatusOK})
 
-	// client := CustomClient(100) // HTTP client
 	client := &MockClient{
 		DoFunc: func(req *http.Request) (*http.Response, error) {
 			return &http.Response{
-				StatusCode: http.StatusAccepted,
+				StatusCode: http.StatusOK,
 			}, nil
 		},
 	}
@@ -58,8 +59,7 @@ func TestOpKey(t *testing.T) {
 		if err != nil {
 			log.Fatal("err=", err)
 		}
-		log.Println(res, err)
-		if res.StatusCode >= 300 {
+		if res.StatusCode != cTest.expectedRes {
 			log.Fatal("status code=", res.StatusCode, "res=", res)
 			errors++
 		}
